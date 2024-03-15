942404088d58d15d961497651c447ae0e3eb4afb
Royer <161404322+Glideroy@users.noreply.github.com>
2024-03-15 18:34:14 -0500

    Update

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,8 +1,5 @@
 import {useState} from 'react'
 
-//createContext para crear el contexto
-//useContext para poder usar ese contexto 
-
 import {useContext} from 'react'
 import {TaskContext} from '../context/TaskContext'
 
@@ -10,46 +7,20 @@ import {TaskContext} from '../context/TaskContext'
 
 export function TaskForm (){
 
-    //const Title = ''
-    const [Title, setTitle] = useState('')
-    //const Descripcion = ''
+    const [Title, setTitle] = useState('')''
     const [Descripcion, setDescripcion] = useState('')
-
-
-     //-------------------USO DE CONTEXTO -----------------------------
-    //Llamamos al contexto qeu vamos a usar
-    //en este caso del contexto TaskContext usaremos su funcion createTask
     const {createTask} = useContext(TaskContext)
-    
-    //------------------------------------------------------------------
-
-
-
-    //contralodr del submid
     const handleSubmid =(e)=>{
-        //resive un evento y .preventDefault(); cancela su comportamiento por defecto (cuando envias un formulario 'submid' por defecto se refresca la pagina)
-        e.preventDefault();
-        
-        //Estamos creando una nueva tarea
         const addTask = {
             title: Title,
             descripcion: Descripcion
         }
         console.log(addTask)
         createTask(addTask)
-        //Estas 2 linea spara que el estado del formulario se limpie en automatico cuando agregue suna tarea
         setTitle('')
         setDescripcion('')
     }
 
-
-
-
-
-    /*
-    value={Title}: el valor sera el mismo que el estado de Title (para que se limpie el formulario por si solo)
-    autoFocus:Cuando cargues la pagina el cursor marcara de forma automatica el imput (listo para escrivir)
-    */
     return(
         <div className='max-w-md mx-auto'>
             <form onSubmit={handleSubmid} className='bg-emerald-500 p-10 mb-4'>

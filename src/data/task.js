0d2d838326f00d06c1abb5d63544f9cd25d64c3d
Royer <161404322+Glideroy@users.noreply.github.com>
2024-03-15 19:08:16 -0500

    Update task.js

--- src/data/task.js
+++ src/data/task.js
@@ -1,22 +1,22 @@
 export const TAREAS =[
     {
         id: 0,
-        title: 'Comprar Alimentos',
-        descripcion: 'Ir al supermercado y comprar los ingredientes necesarios para la semana.'
+        title: 'Refactorización de código',
+        descripcion: 'Revisar y modificar el código existente para mejorar su estructura, legibilidad y mantenibilidad, identificar y eliminar duplicaciones de código, así como refactorizar funciones o componentes complejos en unidades más pequeñas y comprensibles.'
     },
     {
         id: 1,
-        title: 'Hacer ejercicio',
-        descripcion: 'Realizar una rutina de ejercicios cardiovasculares y de fuerza en casa durante 30 minutos.'
+        title: 'Implementación de pruebas automatizadas',
+        descripcion: 'Desarrollar suites de pruebas automatizadas para validar la funcionalidad y la integridad del código. Escribir pruebas unitarias para verificar el comportamiento individual de funciones y componentes, así como pruebas de integración para asegurar la interoperabilidad entre diferentes partes de la aplicación.'
     },
     {
         id: 2,
-        title: 'Llamar a mamá',
-        descripcion: 'Llamar a mamá para saludarla y preguntarle cómo está.'
+        title: 'Optimización de rendimiento',
+        descripcion: 'Identificar y abordar cuellos de botella en el rendimiento de la aplicación, como tiempos de carga lentos, procesamiento intensivo en el lado del cliente o del servidor, y uso excesivo de recursos.'
     },
     {
         id: 3,
-        title: 'Leer un libro',
-        descripcion: 'Dedicar al menos 30 minutos a leer un libro de tu elección para relajarte y aprender algo nuevo.'
+        title: 'Mejora de la accesibilidad',
+        descripcion: ' Evaluar y mejorar la accesibilidad de la aplicación para garantizar que sea usable por personas con discapacidades y cumpla con estándares de accesibilidad como WCAG (Web Content Accessibility Guidelines). '
     }
 ]

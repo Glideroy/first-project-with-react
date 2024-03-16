9563c545ad3d34d3b4823bcaee64c5adb7b4da3d
Royer <161404322+Glideroy@users.noreply.github.com>
2024-03-15 19:08:36 -0500

    Update TaskCard.jsx

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -12,7 +12,7 @@ export function TaskCard({Task}){
         <>
             <div className="bg-gray-800 text-white p-4 rounded-md">
                 <h3 className="text-3xl font-bold capitalize">{Task.title}</h3>
-                <p className="text-gray-400 text-sm">{Task.descripcion}</p>
+                <p className="text-gray-400 text-m">{Task.descripcion}</p>
                 <button 
                 className="bg-blue-400 px-2 py-2 rounded-md mt-4 hover:bg-red-400"
                 onClick={() => {deleteTask(Task.id)}}>
